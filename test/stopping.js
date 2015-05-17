468aa656727f72a697760a4f29cbccdbc1fa2638
Josh Duff <me@JoshDuff.com>
2015-05-16 23:53:48 -0500

    Another test for sanity

---
+++ test/stopping.js
@@ -0,0 +1,24 @@
+var Expirer = require('../')
+var test = require('tap').test
+var level = require('level-mem')
+
+test("stopping actually makes it stop", function(t) {
+	var expirer = new Expirer(500, level('wat'))
+	t.plan(2)
+
+	expirer.on('expire', function() {
+		t.fail('Should not fire after stop is called')
+	})
+
+	expirer.touch('whatever')
+
+	setTimeout(function() {
+		t.pass('stop called')
+		expirer.stop()
+	}, 300)
+
+	setTimeout(function() {
+		t.pass('made it to 1500ms without emitting anything')
+		t.end()
+	}, 1500)
+})
